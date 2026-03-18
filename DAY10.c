244a3bfbda551a8a5fb05d7508fe5471ec435111
Arjun Raj <raj.arjun16@outlook.com>
2026-03-19 03:04:30 +0530

    Add palindrome check using simple loop

---
+++ DAY10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    scanf("%s", s);
+
+    int n = strlen(s);
+
+    for(int i = 0; i < n/2; i++) {
+        if(s[i] != s[n-1-i]) {
+            printf("NO");
+            return 0;
+        }
+    }
+
+    printf("YES");
+    return 0;
+}
