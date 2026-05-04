084922b72c63d1a3e8c8bec6ea15a21f121b3485
Arjun Raj <raj.arjun16@outlook.com>
2026-05-05 01:54:51 +0530

    Implement BST search operation using recursion in C

---
+++ DAY50.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// BST node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST (to build tree)
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Search in BST
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (key < root->data)
+        return search(root->left, key);
+
+    return search(root->right, key);
+}
+
+// Main function
+int main() {
+    struct Node* root = NULL;
+    int n, value, key;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter values:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    printf("Enter value to search: ");
+    scanf("%d", &key);
+
+    struct Node* result = search(root, key);
+
+    if (result != NULL)
+        printf("Element Found\n");
+    else
+        printf("Element Not Found\n");
+
+    return 0;
+}
