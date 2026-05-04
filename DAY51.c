b1c5849d5706cda8167728f380f63f69f48a68d6
Arjun Raj <raj.arjun16@outlook.com>
2026-05-05 01:56:43 +0530

    Add Lowest Common Ancestor (LCA) implementation for BST in C

---
+++ DAY51.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// BST node structure
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insert(root->left, value);
+    else if (value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+// Function to find LCA in BST
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    // If both values are smaller, go left
+    if (n1 < root->data && n2 < root->data)
+        return LCA(root->left, n1, n2);
+
+    // If both values are greater, go right
+    if (n1 > root->data && n2 > root->data)
+        return LCA(root->right, n1, n2);
+
+    // Else current node is LCA
+    return root;
+}
+
+// Main function
+int main() {
+    struct Node* root = NULL;
+    int n, value, n1, n2;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = LCA(root, n1, n2);
+
+    printf("%d\n", lca->data);
+
+    return 0;
+}
